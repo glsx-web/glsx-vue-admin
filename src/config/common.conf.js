9b2667dd7dd15670fdb82e11176a91f763d2459b
huyue <huyue@didihu.com.cn>
2018-10-30 11:44:42 +0800

    错误码

--- src/config/common.conf.js
+++ src/config/common.conf.js
@@ -8,9 +8,15 @@ export default{
       data_key: 'data', // 返回值数据实体存放对象
       message_key: 'message', // 返回信息字段名
       code_key: 'returnCode', // 返回结果字段名
+      errId: 'errId',
       code_success_value: '0', // 返回成功值 注意类型 (字符串与数字)
       code_message_map: { // 错误码 错误信息 映射表
-        '-200': 'reload'
+        '-200': 'reload',
+        '1000': '未知异常，请刷新页面！',
+        '1001': '参数无效，请刷新重试！',
+        '1002': '票据异常，请刷新重试！',
+        '1003': '验证码失效，请重新获取！',
+        '1004': '用户名或密码错误！'
       }
     }
   }
